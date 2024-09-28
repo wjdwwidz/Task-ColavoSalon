98674db47c23f4fc3ea7dbd464300680c0b41937
wjdwwidz <wjdwwidz@gmail.com>
2024-09-28 16:34:57 +0900

    Feat: Apply TypeORM, MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingModule } from './ping/ping.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PingModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['stage.dev.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mongodb',
+          url: configService.get('DB_URL'),
+          port: configService.get('DB_PORT'),
+          autoLoadEntities: true,
+          synchronize: true,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        };
+      },
+    }),
+    PingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
