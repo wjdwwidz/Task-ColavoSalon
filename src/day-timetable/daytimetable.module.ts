6d07507045a3252cbc3b0929231618daa15763c4
wjdwwidz <wjdwwidz@gmail.com>
2024-09-29 16:44:04 +0900

    Feat: Inject all components

--- src/day-timetable/daytimetable.module.ts
+++ src/day-timetable/daytimetable.module.ts
@@ -3,9 +3,20 @@ import { DayTimetableController } from './controllers/daytimetable.controller';
 import { DayTimetableService } from './services/daytimetable.service';
 import { EventRepository } from './repositories/event.repository';
 import { WorkHourRepository } from './repositories/workhour.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WorkHour } from './entities/workhour.entity';
+import { Event } from './entities/event.entity';
+import { TimeSlotService } from './services/timeslot.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WorkHour, Event])],
   controllers: [DayTimetableController],
-  providers: [DayTimetableService, EventRepository, WorkHourRepository],
+  providers: [
+    DayTimetableService,
+    TimeSlotService,
+    EventRepository,
+    WorkHourRepository,
+  ],
+  exports: [WorkHourRepository, EventRepository],
 })
 export class DayTimetableModule {}
