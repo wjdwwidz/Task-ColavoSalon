1adeff9b77603b6fd50a68ee3388bc12a71a9bc5
wjdwwidz <wjdwwidz@gmail.com>
2024-09-29 16:43:54 +0900

    Feat: implement controller

--- src/day-timetable/controllers/daytimetable.controller.ts
+++ src/day-timetable/controllers/daytimetable.controller.ts
@@ -1,18 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import {
   DayTimetableRequest,
   DayTimetableResponse,
 } from '../dtos/get-daytimetable.dto';
 import { DayTimetableService } from '../services/daytimetable.service';
+import { DayTimetable } from '../entities/daytimetable.entity';
 
 @Controller()
 export class DayTimetableController {
   constructor(private readonly dayTimetableService: DayTimetableService) {}
+
   @Post('/getTimeSlots')
-  getDayTimetable(
+  @HttpCode(HttpStatus.CREATED)
+  async getDayTimetable(
     @Body() request: DayTimetableRequest,
-  ): Array<DayTimetableResponse> {
-    const r = this.dayTimetableService.getDayTimetable(request);
-    return null;
+  ): Promise<Array<DayTimetableResponse>> {
+    const dayTimetables =
+      await this.dayTimetableService.getDayTimetables(request);
+    return dayTimetables.map(
+      (dayTimetable: DayTimetable) => new DayTimetableResponse(dayTimetable),
+    );
   }
 }
